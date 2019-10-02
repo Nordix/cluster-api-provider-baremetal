7aa80ad5a1e648a843e47e789c7fdc298119a2c4
Anwar Hassen <anwar.hassen@est.tech>
2019-10-02 11:31:53 +0000

    Created secrets in machine actuator code
    - replaced encoded data with plain text

--- pkg/cloud/baremetal/actuators/machine/actuator.go
+++ pkg/cloud/baremetal/actuators/machine/actuator.go
@@ -44,8 +44,139 @@ const (
 	ProviderName = "baremetal"
 	// HostAnnotation is the key for an annotation that should go on a Machine to
 	// reference what BareMetalHost it corresponds to.
-	HostAnnotation = "metal3.io/BareMetalHost"
-	requeueAfter   = time.Second * 30
+	HostAnnotation      = "metal3.io/BareMetalHost"
+	requeueAfter        = time.Second * 30
+	masterCloudInitData = `#cloud-config
+ssh_authorized_keys:
+  - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkgcW5GQwaIvvZqgv4iIQeCCQhIGtw9p2rz+aJsq4+q4ndGUGkL9urmMIdgWOk6awly/Q14HLmMBBKo5JnG3naqVWIkHB6I7hvsfYHvVchX8p+gM3SPkk+jMEJOD7EI5XZT+SpLw7jI0VVly8eFgQ9owIPfIcrp95R2xYhlnbHqnf+R1uv6LkFl95PLbNElj8d/xZ00aix8MrdiJbraCpngcc1aGNw+OfDa3suraqzj30BCGYC3IETONKvsNWC8+zKoXMG1wqGtQlKIy6yVrGTAYFYj23XyzT9eleIfvQhVJT1R0D0YVd3yoVha4nDjLD8qVYD/SM2kfM7KVlDcrQZ metaldev@metaldeveloper
+
+yum_repos:
+    kubernetes:
+        baseurl: https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+        enabled: 1
+        gpgcheck: 1
+        repo_gpgcheck: 1
+        gpgkey: https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+
+runcmd:
+  - [ ifup, eth1 ]
+  # Install updates
+  - yum check-update
+
+  # Install keepalived
+  - yum install -y gcc kernel-headers kernel-devel
+  - yum install -y keepalived
+  - systemctl start keepalived
+  - systemctl enable keepalived
+
+  # Install docker
+  - yum install -y yum-utils device-mapper-persistent-data lvm2
+  - yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  - yum install docker-ce docker-ce-cli containerd.io -y
+  - usermod -aG docker centos
+  - systemctl start docker
+  - systemctl enable docker
+
+  # Install, Init, Join kubernetes
+  - setenforce 0
+  - sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+  - yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+  - systemctl enable --now kubelet
+  - kubeadm init --token "rjptsr.83zrnxd8yhrnbp8l" --apiserver-advertise-address 192.168.111.249 -v 5
+  - mkdir -p /home/centos/.kube
+  - cp /etc/kubernetes/admin.conf /home/centos/.kube/config
+  - chown centos:centos /home/centos/.kube/config
+  # - kubeadm join 192.168.111.20:6443 --token "rjptsr.83zrnxd8yhrnbp8l" -v 5 --discovery-token-unsafe-skip-ca-verification
+
+# Useful for troubleshooting cloud-init issues
+output: {all: '| tee -a /var/log/cloud-init-output.log'}
+
+# keepalived Configuration file
+write_files:
+  - path: /etc/keepalived/keepalived.conf
+    content: |
+      ! Configuration File for keepalived
+
+      global_defs {
+         notification_email {
+           sysadmin@mydomain.com
+           support@mydomain.com
+         }
+         notification_email_from lb1@mydomain.com
+         smtp_server localhost
+         smtp_connect_timeout 30
+      }
+
+      vrrp_instance VI_1 {
+          state MASTER
+          interface eth0
+          virtual_router_id 51
+          priority 101
+          advert_int 1
+          authentication {
+              auth_type PASS
+              auth_pass 1111
+          }
+          virtual_ipaddress {
+              192.168.111.249
+          }
+      }
+  - path: /etc/sysconfig/network-scripts/ifcfg-eth1
+    owner: root:root
+    permissions: '0644'
+    content: |
+      BOOTPROTO=dhcp
+      DEVICE=eth1
+      ONBOOT=yes
+      TYPE=Ethernet
+      USERCTL=no`
+	workerCloudInitData = `#cloud-config
+
+ssh_authorized_keys:
+  - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkgcW5GQwaIvvZqgv4iIQeCCQhIGtw9p2rz+aJsq4+q4ndGUGkL9urmMIdgWOk6awly/Q14HLmMBBKo5JnG3naqVWIkHB6I7hvsfYHvVchX8p+gM3SPkk+jMEJOD7EI5XZT+SpLw7jI0VVly8eFgQ9owIPfIcrp95R2xYhlnbHqnf+R1uv6LkFl95PLbNElj8d/xZ00aix8MrdiJbraCpngcc1aGNw+OfDa3suraqzj30BCGYC3IETONKvsNWC8+zKoXMG1wqGtQlKIy6yVrGTAYFYj23XyzT9eleIfvQhVJT1R0D0YVd3yoVha4nDjLD8qVYD/SM2kfM7KVlDcrQZ metaldev@metaldeveloper
+
+yum_repos:
+    kubernetes:
+        baseurl: https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+        enabled: 1
+        gpgcheck: 1
+        repo_gpgcheck: 1
+        gpgkey: https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+
+runcmd:
+  - [ ifup, eth1 ]
+  # Install updates
+  - yum check-update
+
+  # Install docker
+  - yum install -y yum-utils device-mapper-persistent-data lvm2
+  - yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  - yum install docker-ce docker-ce-cli containerd.io -y
+  - usermod -aG docker centos
+  - systemctl start docker
+  - systemctl enable docker
+
+  # Install, Init, Join kubernetes
+  - setenforce 0
+  - sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+  - yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+  - systemctl enable --now kubelet
+  - kubeadm join 192.168.111.249:6443 --token "rjptsr.83zrnxd8yhrnbp8l" -v 5 --discovery-token-unsafe-skip-ca-verification
+
+# Useful for troubleshooting cloud-init issues
+output: {all: '| tee -a /var/log/cloud-init-output.log'}
+
+# keepalived Configuration file
+write_files:
+  - path: /etc/sysconfig/network-scripts/ifcfg-eth1
+    owner: root:root
+    permissions: '0644'
+    content: |
+      BOOTPROTO=dhcp
+      DEVICE=eth1
+      ONBOOT=yes
+      TYPE=Ethernet
+      USERCTL=no`
 )
 
 // Add RBAC rules to access cluster-api resources
@@ -76,7 +207,52 @@ func NewActuator(params ActuatorParams) (*Actuator, error) {
 // Create creates a machine and is invoked by the Machine Controller
 func (a *Actuator) Create(ctx context.Context, cluster *machinev1.Cluster, machine *machinev1.Machine) error {
 	log.Printf("Creating machine %v .", machine.Name)
+	log.Printf("********************************************** %v .", machine.Name)
+	//---------------------------------------------Create the secret starts here----------------------------------------------
+	if machine.Name == "master" {
+		masterSecret := &corev1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Secret",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      machine.Name + "-user-data",
+				Namespace: "metal3",
+			},
+			Data: map[string][]byte{
+				"userData": []byte(masterCloudInitData),
+			},
+			Type: "Opaque",
+		}
+		// Create the secret with use data
+		// The reference is set somewhere else.
+		err := a.client.Create(ctx, masterSecret)
 
+		if err != nil {
+			log.Println("Unable to create secret for mmaster")
+		}
+	} else {
+		workerSecret := &corev1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Secret",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      machine.Name + "-user-data",
+				Namespace: "metal3",
+			},
+			Data: map[string][]byte{
+				"userData": []byte(workerCloudInitData),
+			},
+			Type: "Opaque",
+		}
+		err := a.client.Create(ctx, workerSecret)
+
+		if err != nil {
+			log.Println("Unable to create secret for worker")
+		}
+	}
+	//---------------------------------------------Create the secret ends here----------------------------------------------
 	// load and validate the config
 	if machine.Spec.ProviderSpec.Value == nil {
 		return a.setError(ctx, machine, "ProviderSpec is missing")
@@ -132,7 +308,7 @@ func (a *Actuator) Create(ctx context.Context, cluster *machinev1.Cluster, machi
 		return err
 	}
 
-	log.Printf("Finished creating machine %v .", machine.Name)
+	log.Printf("Finished creating machine %v .---+++--++--++--++--++", machine.Name)
 	return nil
 }
 
